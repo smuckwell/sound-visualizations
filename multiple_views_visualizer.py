587b4ea57a6a093abe54556146047a16b11da980
Jason Barnwell <jason.barnwell@smuckwellindustries.com>
2024-11-16 10:08:39 -0800

    Functional platter with waveform, scatter, bar and polar plots.

--- multiple_views_visualizer.py
+++ multiple_views_visualizer.py
@@ -5,6 +5,7 @@ import matplotlib.animation as animation
 import keyboard
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
+from matplotlib.gridspec import GridSpec
 
 # Get the default microphone with loopback enabled
 mic = sc.default_microphone()
@@ -33,6 +34,7 @@ def update_waveform(frame, lines, loopback, text, counter):
     freqs = np.fft.fftfreq(len(fft_data), 1/sample_rate)
     positive_freqs = freqs[:len(freqs)//2]
     positive_fft_data = np.abs(fft_data[:len(fft_data)//2])
+    positive_phases = np.angle(fft_data[:len(fft_data)//2])
 
     dominant_freq = positive_freqs[np.argmax(positive_fft_data)]
 
@@ -52,6 +54,10 @@ def update_waveform(frame, lines, loopback, text, counter):
     lines[2].set_offsets(np.c_[positive_freqs, positive_fft_data])
     lines[2].set_color(color)
 
+    # Update the polar plot
+    lines[3].set_offsets(np.c_[positive_phases, positive_fft_data])
+    lines[3].set_color(color)
+
     # Update the text element every 3 seconds
     if counter[0] % 150 == 0:  # 150 frames * 20ms interval = 3 seconds
         text.set_text(f'Dominant Frequency: {dominant_freq:.2f} Hz')
@@ -62,31 +68,40 @@ def update_waveform(frame, lines, loopback, text, counter):
 # Start recording and display real-time waveform
 print("Press 'q' to quit.")
 with loopback:
-    fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+    fig = plt.figure(figsize=(10, 10))
+    gs = GridSpec(2, 2, figure=fig)
 
     # Waveform plot
+    ax_waveform = fig.add_subplot(gs[0, 0])
     x = np.linspace(0, 4096, 4096)  # Match blocksize
-    line_waveform, = axs[0].plot(x, np.random.rand(4096))  # Match blocksize
-    axs[0].set_ylim(-1, 1)
-    axs[0].axis('off')
+    line_waveform, = ax_waveform.plot(x, np.random.rand(4096))  # Match blocksize
+    ax_waveform.set_ylim(-1, 1)
+    ax_waveform.axis('off')
 
     # Bar plot
+    ax_bar = fig.add_subplot(gs[0, 1])
     bar_freqs = np.fft.fftfreq(4096, 1/sample_rate)[:4096 // 2]
     bar_heights = np.abs(np.fft.fft(np.random.rand(4096))[:4096 // 2])
-    bar_plot = axs[1].bar(bar_freqs, bar_heights, width=10)
-    axs[1].set_xlim(min_freq, max_freq)
-    axs[1].axis('off')
+    bar_plot = ax_bar.bar(bar_freqs, bar_heights, width=10)
+    ax_bar.set_xlim(min_freq, max_freq)
+    ax_bar.axis('off')
 
     # Scatter plot
-    scatter_plot = axs[2].scatter(bar_freqs, bar_heights)
-    axs[2].set_xlim(min_freq, max_freq)
-    axs[2].axis('off')
+    ax_scatter = fig.add_subplot(gs[1, 0])
+    scatter_plot = ax_scatter.scatter(bar_freqs, bar_heights)
+    ax_scatter.set_xlim(min_freq, max_freq)
+    ax_scatter.axis('off')
+
+    # Polar plot
+    ax_polar = fig.add_subplot(gs[1, 1], projection='polar')
+    polar_plot = ax_polar.scatter(np.angle(bar_freqs), bar_heights)
+    ax_polar.set_ylim(0, max(bar_heights))
 
     # Adjust subplot parameters to remove margins
-    fig.subplots_adjust(left=0, right=1, top=1, bottom=0, hspace=0.1)
+    fig.subplots_adjust(left=0, right=1, top=1, bottom=0, hspace=0.1, wspace=0.1)
 
     # Add a text element to display the dominant frequency
-    text = axs[0].text(0.02, 0.93, '', transform=axs[0].transAxes, ha='left', va='top', fontsize=16, color='black')
+    text = ax_waveform.text(0.02, 0.93, '', transform=ax_waveform.transAxes, ha='left', va='top', fontsize=16, color='black')
 
     # Add text to the upper left corner of the visualization
     fig.suptitle(visualization_title, fontsize=16, color='black')
@@ -97,7 +112,7 @@ with loopback:
     # Counter to control the update interval for the text
     counter = [0]
 
-    ani = animation.FuncAnimation(fig, update_waveform, fargs=([line_waveform, bar_plot.patches, scatter_plot], loopback, text, counter), interval=20, cache_frame_data=False)
+    ani = animation.FuncAnimation(fig, update_waveform, fargs=([line_waveform, bar_plot.patches, scatter_plot, polar_plot], loopback, text, counter), interval=20, cache_frame_data=False)
 
     plt.show()
 
