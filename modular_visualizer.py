ba965302b9fad1af7778b2531125120a4f3d4ed1
Jason Barnwell <jason.barnwell@smuckwellindustries.com>
2024-11-16 14:55:53 -0800

    Better visualization swapping.

--- modular_visualizer.py
+++ modular_visualizer.py
@@ -6,6 +6,7 @@ import keyboard
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
 from matplotlib.gridspec import GridSpec
+import math
 
 class AudioVisualizer:
     def __init__(self):
@@ -30,23 +31,16 @@ class AudioVisualizer:
         # Data storage
         self.intensity_array = np.zeros((self.time_window, 4096 // 2))
         self.azimuthal_angle = 150
-        self.current_plot_type = '1'  # Default to waveform plot
+        self.active_plots = []
+        self.axes = []
+        self.texts = []
         
         # Initialize plotting
         self.setup_plot()
         
     def setup_plot(self):
-        self.fig = plt.figure(figsize=(10, 10))
-        self.ax = self.fig.add_subplot(111)
-        self.ax.axis('off')
-        self.fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
-        self.fig.suptitle(self.visualization_title, fontsize=16, color='black')
+        self.fig = plt.figure(figsize=(15, 15))
         plt.get_current_fig_manager().set_window_title(self.visualization_title)
-        
-        # Initialize empty plot
-        self.current_plot = None
-        self.text = self.ax.text(0.02, 0.93, '', transform=self.ax.transAxes,
-                                ha='left', va='top', fontsize=16, color='black')
         self.counter = 0
         
     def get_audio_data(self):
@@ -72,77 +66,114 @@ class AudioVisualizer:
             'color': color
         }
 
-    def plot_waveform(self, data):
-        self.ax.clear()
-        self.ax.plot(np.linspace(0, 4096, 4096), data['audio_data_mono'], color=data['color'])
-        self.ax.set_ylim(-1, 1)
-        self.ax.axis('off')
+    def plot_waveform(self, ax, data):
+        ax.clear()
+        ax.plot(np.linspace(0, 4096, 4096), data['audio_data_mono'], color=data['color'])
+        ax.set_ylim(-1, 1)
+        ax.set_title("Waveform")
+        ax.axis('off')
 
-    def plot_barchart(self, data):
-        self.ax.clear()
-        self.ax.bar(data['positive_freqs'], data['positive_fft_data'],
-                   width=10, color=data['color'])
-        self.ax.set_xlim(self.min_freq, self.max_freq)
-        self.ax.axis('off')
+    def plot_barchart(self, ax, data):
+        ax.clear()
+        ax.bar(data['positive_freqs'], data['positive_fft_data'],
+               width=10, color=data['color'])
+        ax.set_xlim(self.min_freq, self.max_freq)
+        ax.set_title("Frequency Spectrum")
+        ax.axis('off')
 
-    def plot_scatter(self, data):
-        self.ax.clear()
-        self.ax.scatter(data['positive_freqs'], data['positive_fft_data'],
-                       color=data['color'])
-        self.ax.set_xlim(self.min_freq, self.max_freq)
-        self.ax.axis('off')
+    def plot_scatter(self, ax, data):
+        ax.clear()
+        ax.scatter(data['positive_freqs'], data['positive_fft_data'],
+                  color=data['color'])
+        ax.set_xlim(self.min_freq, self.max_freq)
+        ax.set_title("Frequency Scatter")
+        ax.axis('off')
 
-    def plot_polar(self, data):
-        self.ax.clear()
-        self.ax.remove()
-        self.ax = self.fig.add_subplot(111, projection='polar')
+    def plot_polar(self, ax, data):
+        ax.clear()
         marker_sizes = data['positive_fft_data'] * 20
         radial_positions = data['positive_fft_data'] * 20
         polar_colors = self.scalar_map.to_rgba(data['positive_freqs'])
-        self.ax.scatter(data['positive_phases'], radial_positions,
-                       s=marker_sizes, c=polar_colors)
-        self.ax.axis('off')
+        ax.scatter(data['positive_phases'], radial_positions,
+                  s=marker_sizes, c=polar_colors)
+        ax.set_title("Polar Visualization")
+        ax.axis('off')
 
-    def plot_surface(self, data):
-        self.ax.clear()
-        self.ax.remove()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        
-        # Update intensity array
+    def plot_surface(self, ax, data):
+        ax.clear()
         self.intensity_array[:-1] = self.intensity_array[1:]
         self.intensity_array[-1] = data['positive_fft_data']
         
         X, Y = np.meshgrid(data['positive_freqs'], np.arange(self.time_window))
-        self.ax.plot_surface(X, Y, self.intensity_array, cmap='turbo')
-        self.ax.view_init(elev=30, azim=self.azimuthal_angle)
+        ax.plot_surface(X, Y, self.intensity_array, cmap='turbo')
+        ax.view_init(elev=30, azim=self.azimuthal_angle)
         self.azimuthal_angle = (self.azimuthal_angle + 1) % 360
-        self.ax.axis('off')
+        ax.set_title("3D Surface")
+        ax.axis('off')
 
-    def plot_hexbin(self, data):
-        self.ax.clear()
+    def plot_hexbin(self, ax, data):
+        ax.clear()
         x = np.tile(data['positive_freqs'], 10)
         y = np.repeat(data['positive_fft_data'], 10)
-        self.ax.hexbin(x, y, gridsize=30, cmap='turbo')
-        self.ax.set_xlim(self.min_freq, self.max_freq)
-        self.ax.axis('off')
+        ax.hexbin(x, y, gridsize=30, cmap='turbo')
+        ax.set_xlim(self.min_freq, self.max_freq)
+        ax.set_title("Hexbin Density")
+        ax.axis('off')
+
+    def reorganize_layout(self):
+        n = len(self.active_plots)
+        if n == 0:
+            return
+        
+        # Calculate grid dimensions
+        cols = math.ceil(math.sqrt(n))
+        rows = math.ceil(n / cols)
+        
+        # Clear the figure
+        self.fig.clear()
+        self.axes = []
+        gs = GridSpec(rows, cols, figure=self.fig)
+        
+        # Create new subplots
+        for i, plot_type in enumerate(self.active_plots):
+            row = i // cols
+            col = i % cols
+            
+            if plot_type == '4':  # Polar plot
+                ax = self.fig.add_subplot(gs[row, col], projection='polar')
+            elif plot_type == '5':  # Surface plot
+                ax = self.fig.add_subplot(gs[row, col], projection='3d')
+            else:
+                ax = self.fig.add_subplot(gs[row, col])
+            
+            self.axes.append(ax)
+        
+        # Add title
+        if self.active_plots:
+            self.fig.suptitle(self.visualization_title, fontsize=16, y=0.98)
+        
+        # Adjust layout
+        self.fig.tight_layout()
 
     def check_keyboard(self):
         for key in ['1', '2', '3', '4', '5', '6']:
             if keyboard.is_pressed(key):
-                self.current_plot_type = key
-                # Reset axis for new plot type
-                self.ax.clear()
-                self.ax.remove()
-                self.ax = self.fig.add_subplot(111)
-                if key == '4':  # Polar plot needs polar projection
-                    self.ax.remove()
-                    self.ax = self.fig.add_subplot(111, projection='polar')
-                elif key == '5':  # Surface plot needs 3D projection
-                    self.ax.remove()
-                    self.ax = self.fig.add_subplot(111, projection='3d')
+                if key not in self.active_plots:
+                    self.active_plots.append(key)
+                    self.reorganize_layout()
+                    plt.pause(0.2)  # Debounce
+        
+        if keyboard.is_pressed('backspace'):
+            if self.active_plots:
+                self.active_plots.pop()
+                self.reorganize_layout()
+                plt.pause(0.2)  # Debounce
 
     def update(self, frame):
         self.check_keyboard()
+        if not self.active_plots:
+            return []
+        
         data = self.get_audio_data()
         
         plot_functions = {
@@ -154,23 +185,21 @@ class AudioVisualizer:
             '6': self.plot_hexbin
         }
         
-        plot_functions[self.current_plot_type](data)
+        for ax, plot_type in zip(self.axes, self.active_plots):
+            plot_functions[plot_type](ax, data)
         
-        # Update frequency text every second
-        if self.counter % 50 == 0:
-            self.text.set_text(f'Dominant Frequency: {data["dominant_freq"]:.2f} Hz')
         self.counter += 1
-        
-        return self.ax,
+        return self.axes
 
     def run(self):
-        print("Press 1-6 to switch visualizations:")
+        print("Press 1-6 to add visualizations:")
         print("1: Waveform")
         print("2: Bar Chart")
         print("3: Scatter Plot")
         print("4: Polar Plot")
         print("5: Surface Plot")
         print("6: Hexbin Plot")
+        print("Backspace: Remove last visualization")
         print("Press 'q' to quit.")
         
         with self.loopback:
